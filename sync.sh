57f094c75cf068f6a72cf1057ade9e767ee8505f
LwithM2 <122188293+LwithM2@users.noreply.github.com>
2023-01-12 15:20:10 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,16 +7,17 @@ mkdir -p $SYNC_PATH
 cd $SYNC_PATH
 
 # Init Repo
-repo init -u $MANIFEST -b $MANIFEST_BRANCH
+repo init --depth=1 -u $MANIFEST -b $MANIFEST_BRANCH
 
 # Sync the Sources
-repo sync
+repo sync repo sync -j$(nproc --all) --force-sync
 
 # Clone Trees
 git clone --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
-git clone --depth=1 $VT_LINK $VT_PATH
-git clone --depth=1 $KT_LINK $KT_PATH
-git clone --depth=1 $CO_LINK $CO_PATH
+git clone --depth=1 $VT_LINK $VT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
+git clone --depth=1 $KT_LINK $KT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
+git clone --depth=1 $CO_LINK $CO_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
+git clone --depth=1 $VT_LINK $VT_CO_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
 
 # Custom
 rm -rf hardware/xiaomi
