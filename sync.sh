31bae4d826f314292f933be2b0920710d3204a33
LwithM2 <122188293+LwithM2@users.noreply.github.com>
2023-01-12 15:41:50 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -17,7 +17,6 @@ git clone --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Devi
 git clone --depth=1 $VT_LINK $VT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
 git clone --depth=1 $KT_LINK $KT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
 git clone --depth=1 $CO_LINK $CO_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
-git clone --depth=1 $VT_LINK $VT_CO_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
 
 # Custom
 rm -rf hardware/xiaomi
@@ -27,6 +26,7 @@ rm -rf hardware/google/pixel/kernel_headers
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
 git clone --depth=1 https://github.com/Jabiyeff-Project/android_hardware_qcom_wlan.git -b 13.0-spes hardware/qcom-caf/wlan
 git clone --depth=1 https://github.com/ArrowOS/android_hardware_qcom_bootctrl.git -b arrow-13.0-caf hardware/qcom-caf/bootctrl
+git clone --depth=1 https://github.com/Mi-Thorium/proprietary_vendor_xiaomi_mithorium-common.git -b a12/master vendor/xiaomi/mithorium-common
 
 # Exit
 exit 0
