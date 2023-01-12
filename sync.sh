d060d2c4141b52f197622664d09badbaf06a1912
LwithM2 <122188293+LwithM2@users.noreply.github.com>
2023-01-12 16:50:51 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,7 +7,7 @@ mkdir -p $SYNC_PATH
 cd $SYNC_PATH
 
 # Init Repo
-repo init --depth=1 -u $MANIFEST -b $MANIFEST_BRANCH
+repo init -u $MANIFEST -b $MANIFEST_BRANCH
 
 # Sync the Sources
 repo sync -j$(nproc --all) --force-sync
@@ -19,13 +19,6 @@ git clone --depth=1 $KT_LINK $KT_PATH || { echo "ERROR: Failed to Clone the Devi
 git clone --depth=1 $CO_LINK $CO_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
 
 # Custom
-rm -rf hardware/xiaomi
-rm -rf hardware/qcom-caf/wlan
-rm -rf hardware/qcom-caf/bootctrl
-rm -rf hardware/google/pixel/kernel_headers
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
-git clone --depth=1 https://github.com/Jabiyeff-Project/android_hardware_qcom_wlan.git -b 13.0-spes hardware/qcom-caf/wlan
-git clone --depth=1 https://github.com/ArrowOS/android_hardware_qcom_bootctrl.git -b arrow-13.0-caf hardware/qcom-caf/bootctrl
 git clone --depth=1 https://github.com/Mi-Thorium/proprietary_vendor_xiaomi_mithorium-common.git -b a12/master vendor/xiaomi/mithorium-common
 
 # Exit
