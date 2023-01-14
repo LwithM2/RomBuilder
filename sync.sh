f8a3a03d4a0970362021032aef7d7783b1915b95
LwithM2 <122188293+LwithM2@users.noreply.github.com>
2023-01-14 12:56:24 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,19 +2,27 @@
 
 ls
 
-
-# Make the Directory if it doesn't exist
-mkdir -p work
-
-# Change to the Source Directory
-cd work
+#sudo apt install curl
+#curl -X POST "https://api.telegram.org/bot5838326569:AAHDBk9CwwAgRUu1f97lD9Oi1jTaNcXCUFU/sendMessage" -d "chat_id=5202036980&text=开始安装所需依赖..."
+#sudo apt update
+#sudo apt -y upgrade
+#sudo apt -y install git-core gnupg flex bison gperf zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip python make openjdk-8-jdk 
+#mkdir ~/bin
+#curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+#chmod a+x ~/bin/repo
+#sudo ln -sf ~/bin/repo /usr/bin/repo
+#mkdir workspace
+#cd workspace
+#echo "workspace-folder=$(pwd)" >> $GITHUB_OUTPUT
+#git config --global user.name "Captain Throwback"
+#git config --global user.email "captainthrowback@hotmail.com"
 
 # Init Repo
-repo init -u $MANIFEST -b $MANIFEST_BRANCH
+repo init -u https://github.com/Project-Awaken/android_manifest.git -b 12.1
 git clone https://github.com/lanmiemie/local_manifests.git --depth 1 -b patch-1 .repo/local_manifests
 
 # Sync the Sources
-curl -X POST "https://api.telegram.org/bot5838326569:AAHDBk9CwwAgRUu1f97lD9Oi1jTaNcXCUFU/sendMessage" -d "chat_id=5202036980&text=开始同步 $MANIFEST_BRANCH 分支代码"
+curl -X POST "https://api.telegram.org/bot5838326569:AAHDBk9CwwAgRUu1f97lD9Oi1jTaNcXCUFU/sendMessage" -d "chat_id=5202036980&text=开始同步 12.1 分支代码"
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # Clone Trees
