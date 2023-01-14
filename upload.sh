70c745d9d0cb12b96d714a9058fac95f8074f86e
LwithM2 <122188293+LwithM2@users.noreply.github.com>
2023-01-14 13:26:45 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -9,21 +9,20 @@ echo "Uploading the Build..."
 echo "============================"
 
 # Change to the Output Directory
-cd out/target/product/${DEVICE}
+cd out/target/product/Mi439
 
 # List the Files
-echo "Files inside ${SYNC_PATH}/out/target/product/${DEVICE} are:"
+echo "Files inside workspace/out/target/product/mi439 are:"
 ls --color=auto
 
 # Set FILENAME var
-FILENAME=$(echo $OUTPUT)
+FILENAME=Awaken*.zip
 
 # Upload to oshi.at
-if [ -z "$TIMEOUT" ];then
-    TIMEOUT=20160
-fi
+TIMEOUT=20160
 
-curl -T $FILENAME https://oshi.at/${FILENAME}/${TIMEOUT} | tee link.txt > /dev/null || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
+
+curl -T $FILENAME https://oshi.at/Awaken*.zip/${TIMEOUT} | tee link.txt > /dev/null || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
 
 MIRROR_LINK=$(cat link.txt | grep Download | cut -d\  -f1)
 
